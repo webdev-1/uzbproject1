e48907d137ddaa86fbb0234e0ab026a331c04d75
Seo Ho Yi <msyi1995@naver.com>
2019-12-27 17:48:54 -0500

    initialize fb

--- react-boilerplate-master/app/containers/HomePage/index.js
+++ react-boilerplate-master/app/containers/HomePage/index.js
@@ -33,6 +33,10 @@ import { makeSelectUsername } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
+// firebase import
+import firebase from '../../fbconfig';
+import {firestore} from '../../fbconfig';
+
 const key = 'home';
 
 export function HomePage({
@@ -48,6 +52,10 @@ export function HomePage({
 
   useEffect(() => {
     // When initial state username is not null, submit the form to load repos
+    firestore.collection("users").get().then((snapshot )=> {
+      console.log(snapshot.docs);
+    });
+
     if (username && username.trim().length > 0) onSubmitForm();
   }, []);
 
